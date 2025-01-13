67ca05155cdba0f47c7d86780617950b8c062624
h0angpc <118001661+h0angpc@users.noreply.github.com>
2025-01-13 22:12:56 +0700

    refactor: rename ui package to presentation and remove unused code in MainActivity

--- Challenge 1/CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/presentation/theme/Type.kt
+++ Challenge 1/CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.currencyconverterapp.ui.theme
+package com.example.currencyconverterapp.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
