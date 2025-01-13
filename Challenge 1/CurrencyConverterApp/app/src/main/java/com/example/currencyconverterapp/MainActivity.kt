1e2f383235586d5c97693f449250108e75df1710
h0angpc <118001661+h0angpc@users.noreply.github.com>
2025-01-13 22:41:14 +0700

    add: add 2 card currencyRow to MainScreen

--- Challenge 1/CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
+++ Challenge 1/CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.currencyconverterapp.presentation.main_screen.MainScreen
 import com.example.currencyconverterapp.presentation.theme.CurrencyConverterAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CurrencyConverterAppTheme {
-
+                MainScreen()
             }
         }
     }
--- Challenge 1/CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/presentation/main_screen/MainScreen.kt
+++ Challenge 1/CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/presentation/main_screen/MainScreen.kt
@@ -1,10 +1,19 @@
 package com.example.currencyconverterapp.presentation.main_screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -20,12 +29,79 @@ fun MainScreen(){
             .padding(horizontal = 16.dp, vertical = 8.dp)
     ) {
         Text(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxWidth(),
             text = "ConverterApp",
             fontFamily = FontFamily.Cursive,
             fontSize = 40.sp,
             textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
         )
+        Card(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp, vertical = 4.dp),
+                horizontalAlignment = Alignment.End
+            )
+            {
+                CurrencyRow(
+                    modifier = Modifier.fillMaxWidth(),
+                    currencyCode = "VND",
+                    currencyName = "Viet Nam Dong",
+                    onDropDownIconClicked = {}
+                )
+                Text(
+                    text = "20000",
+                    fontSize = 40.sp
+                )
+            }
+        }
+        Card(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp, vertical = 4.dp),
+                horizontalAlignment = Alignment.End
+            )
+            {
+                Text(
+                    text = "20000",
+                    fontSize = 40.sp
+                )
+                CurrencyRow(
+                    modifier = Modifier.fillMaxWidth(),
+                    currencyCode = "VND",
+                    currencyName = "Viet Nam Dong",
+                    onDropDownIconClicked = {}
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrencyRow(
+    modifier: Modifier = Modifier,
+    currencyCode: String,
+    currencyName: String,
+    onDropDownIconClicked: () -> Unit
+) {
+    Row (
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = currencyCode, fontSize = 14.sp, fontWeight = FontWeight.Bold)
+        IconButton(onClick = onDropDownIconClicked) {
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Open bottom sheet"
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = currencyName, fontSize = 14.sp, fontWeight = FontWeight.Bold)
     }
 }
