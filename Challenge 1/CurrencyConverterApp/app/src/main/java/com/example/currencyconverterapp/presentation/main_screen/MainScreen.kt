f6fb2bae7a52160c6e6664b1c17cae036c15575b
h0angpc <118001661+h0angpc@users.noreply.github.com>
2025-01-13 23:05:12 +0700

    add: add sync icon for mainScreen

--- Challenge 1/CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/presentation/main_screen/MainScreen.kt
+++ Challenge 1/CurrencyConverterApp/app/src/main/java/com/example/currencyconverterapp/presentation/main_screen/MainScreen.kt
@@ -1,25 +1,34 @@
 package com.example.currencyconverterapp.presentation.main_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.currencyconverterapp.R
 
 @Composable
 fun MainScreen(){
@@ -79,6 +88,23 @@ fun MainScreen(){
                     onDropDownIconClicked = {}
                 )
             }
+        }
+        Box(
+            modifier = Modifier
+                .padding(start = 40.dp)
+                .clip(CircleShape)
+                .clickable {  }
+                .background(color = MaterialTheme.colorScheme.background)
+        )
+        {
+            Icon(
+                painter = painterResource(R.drawable.ic_sync),
+                contentDescription = "Swap Currency",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(25.dp),
+                tint = MaterialTheme.colorScheme.onSurface
+            )
         }
     }
 }
